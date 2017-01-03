10fa83a353d6a5f4eb02f39379a0e5777e7a7ad8
Cameron <cameron.w.james@live.com>
2017-01-03 07:42:22 +0000

    create basic tournament#show view, change char limit for team name

--- app/validators/team_names_validator.rb
+++ app/validators/team_names_validator.rb
@@ -1,7 +1,7 @@
 class TeamNamesValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     regexp = /[\+=_\)\(\*&^%$@!~\{\}\[\]-]/
-    record.errors[attribute] << "must be between 3 and 36 characters" if value.to_a.any?{ |name| name.length < 3 || name.length > 36}
+    record.errors[attribute] << "must be between 3 and 24 characters" if value.to_a.any?{ |name| name.length < 3 || name.length > 24}
     record.errors[attribute] << "can not include characters +=-_\*&^%$@!~{}[]" if value.any?{ |name| regexp =~ name }
     record.errors[attribute] << "must all be different" if value.map(&:downcase) != (value.map(&:downcase)).uniq
     record.errors[attribute] << "must have one name for every team" if value.length != record.num_teams
